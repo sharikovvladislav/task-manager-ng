158929303fcf89c2dc325ee3b27097ea220226b4
Sharikov Vladislav <vlad.spb.sh@gmail.com>
2016-05-10 23:12:22 +0300

    Fix line length

--- public/js/common/directives/directives.js
+++ public/js/common/directives/directives.js
@@ -1,5 +1,8 @@
-define(['angular', 'common/directives/task-type-adder', 'common/directives/validate-form'], function (angular, taskTypeAdder, validateForm) {
-  var module = angular.module('common.directives', []);
-  taskTypeAdder(module);
-  validateForm(module);
-});
+define(
+  ['angular', 'common/directives/task-type-adder', 'common/directives/validate-form'],
+  function (angular, taskTypeAdder, validateForm) {
+    var module = angular.module('common.directives', []);
+    taskTypeAdder(module);
+    validateForm(module);
+  }
+);
