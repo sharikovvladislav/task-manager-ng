f9e5a148c6a947d820c38cd9cd6a5d8685671e54
Sharikov Vladislav <vlad.spb.sh@gmail.com>
2016-05-10 22:01:50 +0300

    Add simple test

---
+++ tests/unit/tm/tm-add.spec.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Vlad on 10.05.2016.
+ */
+'use strict';
+
+define(function () {
+  describe('test case #1', function () {
+    it('test #1', function () {
+      expect(true).toEqual(true);
+    });
+  });
+});
