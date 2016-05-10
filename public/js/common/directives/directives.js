07a683ce83793f3341de13603c8dc1d76470cc32
Sharikov Vladislav <vlad.spb.sh@gmail.com>
2016-05-10 23:09:26 +0300

    Refactor RequireJS deps

--- public/js/common/directives/directives.js
+++ public/js/common/directives/directives.js
@@ -1,5 +1,4 @@
-var deps = ['angular', 'common/directives/task-type-adder', 'common/directives/validate-form'];
-define(deps, function (angular, taskTypeAdder, validateForm) {
+define(['angular', 'common/directives/task-type-adder', 'common/directives/validate-form'], function (angular, taskTypeAdder, validateForm) {
   var module = angular.module('common.directives', []);
   taskTypeAdder(module);
   validateForm(module);
