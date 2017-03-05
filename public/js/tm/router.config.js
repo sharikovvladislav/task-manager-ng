3c05177dc1082268080769f359e502a50da390a0
Alina <spb.alina.93@gmail.com>
2017-03-05 20:58:08 +0300

    Организовал stateProvider конфигурацию в разных файлах

--- public/js/tm/router.config.js
+++ public/js/tm/router.config.js
@@ -1,5 +1,5 @@
 define(['tm/controllers/controllers', 'text!tm/views/task-manager.html'], function (ctrl, view) {
-  return angular.module('settings.router.config', ['ui.router', 'tm.controllers'])
+  return angular.module('tm.router.config', ['ui.router', 'tm.controllers'])
       .config(function ($stateProvider) {
         $stateProvider.state({
           name: 'tm',
