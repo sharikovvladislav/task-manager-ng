da1ab726342e4948baae5ad5c366c38df11fcc3d
Sharikov Vladislav <vlad.spb.sh@gmail.com>
2017-03-06 23:19:56 +0300

    Вынес menu в компонент

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
 define([
-  'angular', 'window', 'main/main', 'common/common', 'tm/tm', 'settings/settings'
+  'angular', 'window', 'main/main', 'common/common', 'tm/tm', 'settings/settings', 'menu/menu.component'
 ], function (angular, window) {
   'use strict';
 
   var dependencies = angular.module('project.dependencies', ['dndLists', 'ui.bootstrap', 'ui.router']),
-    app = angular.module('myApp', ['project.dependencies', 'ngRoute', 'common', 'main', 'tm', 'settings']);
+    app = angular.module('myApp', ['project.dependencies', 'ngRoute', 'common', 'main', 'tm', 'settings', 'menu.component']);
 
   app.init = function () {
     angular.bootstrap(window.document, ['myApp']);
---
+++ public/js/menu/menu.component.js
@@ -0,0 +1,26 @@
+define([
+  'text!menu/menu.html'
+], function (menuTemplate) {
+  'use strict';
+  var angular = require('angular');
+
+  MenuComponentCtrl.$inject = ['$scope'];
+  function MenuComponentCtrl ($scope) {
+    $scope.model = {};
+    $scope.model.projectName = 'abc';
+
+    $scope.model.leftMenu = [
+      {label: 'Task manager', sref: 'tm'}
+    ];
+
+    $scope.model.rightMenu = [
+      {label: 'Settings', sref: 'settings'}
+    ];
+  }
+
+  return angular.module('menu.component', [])
+    .component('menuComponent', {
+      controller: MenuComponentCtrl,
+      template: menuTemplate
+    });
+});
