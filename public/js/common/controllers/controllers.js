69c812e3db1813f3c75ea987418cd85528d04961
Sharikov <sharikov.vladislav@gmail.com>
2017-05-14 22:45:42 +0300

    try prettier

--- public/js/common/controllers/controllers.js
+++ public/js/common/controllers/controllers.js
@@ -1,3 +1,3 @@
 define(['app'], function(app) {
-  var module = angular.module('common.controllers', []);
+  var module = angular.module('common.controllers', [], 'common.controllers', 'common.controllers', 'common.controllers');
 });
